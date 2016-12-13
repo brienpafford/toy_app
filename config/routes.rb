93096de430e3280de014b9a29a4ea229fca4f2c9
bpafford <brien.pafford@metova.com>
2016-12-13 15:49:18 -0600

    Microposts Scaffolding, database migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-
+  resources :microposts
   resources :users
   root 'application#hello'
 end
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
